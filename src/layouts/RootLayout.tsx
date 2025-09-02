f9953bed88728ec32baa388d4b486f1d482baa39
Tnalxmsk <alstnwkd990@naver.com>
2025-09-02 21:29:03 +0900

    init: router 설정

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+import { Outlet } from "react-router";
+
+const RootLayout = () => {
+  return (
+    <Wrapper>
+      <Outlet />
+    </Wrapper>
+  );
+};
+
+export default RootLayout;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100dvh;
+  background-color: white;
+  padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+`;
