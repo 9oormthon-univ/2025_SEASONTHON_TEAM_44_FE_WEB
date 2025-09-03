5be139c3cee0f6a5402dfe9f1b1863a2d9f5e899
Tnalxmsk <alstnwkd990@naver.com>
2025-09-04 02:18:15 +0900

    :art: style: 이전 메세지 UI 구현

--- src/pages/NoticesPage.tsx
+++ src/pages/NoticesPage.tsx
@@ -2,20 +2,32 @@ import NoticesTab from "@components/notices/NoticesTab.tsx";
 import NoticesForm from "@components/notices/NoticesForm.tsx";
 import styled from "@emotion/styled";
 import { useState } from "react";
+import MessageHistory from "@components/notices/MessageHistory.tsx";
+import ListPageAction from "@components/common/ListPageAction.tsx";
+import { useHandleListPageNumber } from "@hooks/visit/useHandleListPageNumber.ts";
+
+const PAGE_SIZE = 5;
 
 const NoticesPage = () => {
   const [currentTab, setCurrentTab] = useState<"send" | "history">("send");
+  const { currentPage, onChangePage } = useHandleListPageNumber();
   return (
-    <NoticesContainer>
+    <NoticesContainer currentTab={currentTab}>
       <NoticesTab currentTab={currentTab} setCurrentTab={setCurrentTab} />
-      <NoticesForm />
+      {currentTab === "send" && <NoticesForm />}
+      {currentTab === "history" && (
+        <NoticesMessageHistoryWrapper>
+          <MessageHistory />
+          <ListPageAction currentPage={currentPage} onChangePage={onChangePage} pageSize={PAGE_SIZE} />
+        </NoticesMessageHistoryWrapper>
+      )}
     </NoticesContainer>
   );
 };
 
 export default NoticesPage;
 
-const NoticesContainer = styled.div`
+const NoticesContainer = styled.div<{ currentTab: string }>`
   display: flex;
   flex-direction: column;
   gap: 20px;
@@ -24,10 +36,17 @@ const NoticesContainer = styled.div`
   padding: 52px 210px;
   align-items: center;
   min-width: 1240px;
-  
+
   @media (max-width: 1240px) {
-    width: 100%;
-    min-width: 500px;
-    padding: 52px 10%;
+    width: ${({ currentTab }) => currentTab === "send" ? "100%" : undefined};
+    min-width: ${({ currentTab }) => currentTab === "send" ? "100%" : undefined};
+    padding: ${({ currentTab }) => currentTab === "send" ? "52px 10%" : "52px 210px"};
   }
+`;
+
+const NoticesMessageHistoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
 `;
