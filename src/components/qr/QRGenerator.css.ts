edbc09359ea7717546c1d3e40e9801f96681a0a8
Tnalxmsk <alstnwkd990@naver.com>
2025-09-03 16:44:32 +0900

    :art: style: QR 관리 페이지 UI 구현

---
+++ src/components/qr/QRGenerator.css.ts
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+
+const QRGeneratorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.white};
+  align-items: center;
+  border-radius: 20px;
+  padding: 60px 10%;
+  text-align: center;
+  white-space: pre-wrap;
+  min-width: 500px;
+  gap: 20px;
+`;
+
+const QRGeneratorTitle = styled.div`
+  font: ${({ theme }) => theme.fonts.h2};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const QRGeneratorPrimaryText = styled.div`
+  font: ${({ theme }) => theme.fonts.sub1};
+  color: ${({ theme }) => theme.colors.primary.primary500};
+`;
+
+const QRGeneratorDescription = styled.div`
+  font: ${({ theme }) => theme.fonts.body1};
+  color: ${({ theme }) => theme.colors.grayScale.gray800};
+`;
+
+const QRGeneratorButton = styled.button`
+  padding: 17px 131px;
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.primary.primary500};
+  color: ${({ theme }) => theme.colors.white};
+  font: ${({ theme }) => theme.fonts.button1};
+  border: none;
+  cursor: pointer;
+`;
+
+export { QRGeneratorContainer, QRGeneratorTitle, QRGeneratorPrimaryText, QRGeneratorDescription, QRGeneratorButton };
