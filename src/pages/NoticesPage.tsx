63c74ebc59c5577bfd2001a0b7c42a6304aeb5b5
Tnalxmsk <alstnwkd990@naver.com>
2025-09-04 01:49:08 +0900

    :art: style: 공지 페이지 UI 일부 구현

--- src/pages/NoticesPage.tsx
+++ src/pages/NoticesPage.tsx
@@ -1,7 +1,33 @@
+import NoticesTab from "@components/notices/NoticesTab.tsx";
+import NoticesForm from "@components/notices/NoticesForm.tsx";
+import styled from "@emotion/styled";
+import { useState } from "react";
+
 const NoticesPage = () => {
+  const [currentTab, setCurrentTab] = useState<"send" | "history">("send");
   return (
-    <div>NoticesPage</div>
+    <NoticesContainer>
+      <NoticesTab currentTab={currentTab} setCurrentTab={setCurrentTab} />
+      <NoticesForm />
+    </NoticesContainer>
   );
 };
 
 export default NoticesPage;
+
+const NoticesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 20px;
+  padding: 52px 210px;
+  align-items: center;
+  min-width: 1240px;
+  
+  @media (max-width: 1240px) {
+    width: 100%;
+    min-width: 500px;
+    padding: 52px 10%;
+  }
+`;
