89bda54b0b84f0d6be663f796bf659246b3358da
Tnalxmsk <alstnwkd990@naver.com>
2025-09-04 14:24:55 +0900

    :recycle: refactor: 컴포넌트 css 코드 분리

---
+++ src/components/notices/NoticesForm.css.ts
@@ -0,0 +1,82 @@
+import styled from "@emotion/styled";
+
+const NoticesFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  width: 100%;
+`;
+
+const NoticesFormInputSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const NoticesFormInputLabel = styled.label`
+  font: ${({ theme }) => theme.fonts.body1};
+  color: ${({ theme }) => theme.colors.black};
+`;
+
+const NoticesFormInput = styled.input`
+  padding: 20px;
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.black};
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.grayScale.gray30};;
+  border: none;
+  outline: none;
+  width: 402px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grayScale.gray200};
+  }
+
+  &:hover {
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.12);
+    outline: none;
+  }
+
+  &:focus-visible {
+    outline: none;
+    border: none;
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.12);
+  }
+
+  transition: box-shadow 0.15s ease, background 0.15s ease;
+`;
+
+const NoticesFormTextArea = styled.textarea`
+  padding: 20px;
+  font: ${({ theme }) => theme.fonts.body2};
+  color: ${({ theme }) => theme.colors.black};
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.grayScale.gray30};;
+  border: none;
+  outline: none;
+  min-height: 69px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grayScale.gray200};
+  }
+`;
+
+const NoticesFormButton = styled.div`
+  margin-left: auto;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding: 17px 146px;
+  border-radius: 12px;
+  background-color: ${({ theme }) => theme.colors.primary.primary500};
+  color: ${({ theme }) => theme.colors.white};
+  font: ${({ theme }) => theme.fonts.button1};
+  border: none;
+  cursor: pointer;
+
+  @media (max-width: 968px) {
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+export { NoticesFormContainer, NoticesFormInputSection, NoticesFormInputLabel, NoticesFormInput, NoticesFormTextArea, NoticesFormButton };
--- src/components/notices/NoticesForm.tsx
+++ src/components/notices/NoticesForm.tsx
@@ -1,6 +1,6 @@
-import styled from "@emotion/styled";
 import ReceiverSelector from "@components/notices/ReceiverSelector.tsx";
 import { useState } from "react";
+import * as S from "@components/notices/NoticesForm.css.ts";
 
 const receiverOptions = [
   { value: 'all', label: '전체' },
@@ -12,109 +12,30 @@ const NoticesForm = () => {
   const [receiver, setReceiver] = useState<string | null>(null);
 
   return (
-    <NoticesFormContainer>
+    <S.NoticesFormContainer>
       {/* 메세지 제목 */}
-      <NoticesFormInputSection>
-        <NoticesFormInputLabel>제목</NoticesFormInputLabel>
-        <NoticesFormInput type="text" placeholder="제목을 입력하세요" />
-      </NoticesFormInputSection>
+      <S.NoticesFormInputSection>
+        <S.NoticesFormInputLabel>제목</S.NoticesFormInputLabel>
+        <S.NoticesFormInput type="text" placeholder="제목을 입력하세요" />
+      </S.NoticesFormInputSection>
       {/* 메세지 수신자 */}
-      <NoticesFormInputSection>
-        <NoticesFormInputLabel>받는 사람</NoticesFormInputLabel>
+      <S.NoticesFormInputSection>
+        <S.NoticesFormInputLabel>받는 사람</S.NoticesFormInputLabel>
         <ReceiverSelector
           placeholder="받는 사람을 선택하세요"
           options={receiverOptions}
           value={receiver}
           onChange={setReceiver}
         />
-      </NoticesFormInputSection>
+      </S.NoticesFormInputSection>
       {/* 메세지 본문 */}
-      <NoticesFormInputSection>
-        <NoticesFormInputLabel>본문</NoticesFormInputLabel>
-        <NoticesFormTextArea placeholder="본문을 입력하세요" />
-      </NoticesFormInputSection>
-      <NoticesFormButton>공지 보내기</NoticesFormButton>
-    </NoticesFormContainer>
+      <S.NoticesFormInputSection>
+        <S.NoticesFormInputLabel>본문</S.NoticesFormInputLabel>
+        <S.NoticesFormTextArea placeholder="본문을 입력하세요" />
+      </S.NoticesFormInputSection>
+      <S.NoticesFormButton>공지 보내기</S.NoticesFormButton>
+    </S.NoticesFormContainer>
   );
 };
 
 export default NoticesForm;
-
-const NoticesFormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  width: 100%;
-`;
-
-const NoticesFormInputSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-`;
-
-const NoticesFormInputLabel = styled.label`
-  font: ${({ theme }) => theme.fonts.body1};
-  color: ${({ theme }) => theme.colors.black};
-`;
-
-const NoticesFormInput = styled.input`
-  padding: 20px;
-  font: ${({ theme }) => theme.fonts.body2};
-  color: ${({ theme }) => theme.colors.black};
-  border-radius: 12px;
-  background-color: ${({ theme }) => theme.colors.grayScale.gray30};;
-  border: none;
-  outline: none;
-  width: 402px;
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.grayScale.gray200};
-  }
-
-  &:hover {
-    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.12);
-    outline: none;
-  }
-
-  &:focus-visible {
-    outline: none;
-    border: none;
-    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.12);
-  }
-
-  transition: box-shadow 0.15s ease, background 0.15s ease;
-`;
-
-const NoticesFormTextArea = styled.textarea`
-  padding: 20px;
-  font: ${({ theme }) => theme.fonts.body2};
-  color: ${({ theme }) => theme.colors.black};
-  border-radius: 12px;
-  background-color: ${({ theme }) => theme.colors.grayScale.gray30};;
-  border: none;
-  outline: none;
-  min-height: 69px;
-
-  &::placeholder {
-    color: ${({ theme }) => theme.colors.grayScale.gray200};
-  }
-`;
-
-const NoticesFormButton = styled.div`
-  margin-left: auto;
-  margin-top: 20px;
-  margin-bottom: 20px;
-  padding: 17px 146px;
-  border-radius: 12px;
-  background-color: ${({ theme }) => theme.colors.primary.primary500};
-  color: ${({ theme }) => theme.colors.white};
-  font: ${({ theme }) => theme.fonts.button1};
-  border: none;
-  cursor: pointer;
-
-  @media (max-width: 968px) {
-    width: 100%;
-    text-align: center;
-  }
-`;
