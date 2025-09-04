d4afcef5973db521b7b0abbc0e2847b91a41e405
Tnalxmsk <alstnwkd990@naver.com>
2025-09-04 14:52:03 +0900

    :sparkles: feat: 폼 기능 구현

--- src/components/notices/NoticesForm.tsx
+++ src/components/notices/NoticesForm.tsx
@@ -1,22 +1,16 @@
 import ReceiverSelector from "@components/notices/ReceiverSelector.tsx";
-import { useState } from "react";
 import * as S from "@components/notices/NoticesForm.css.ts";
-
-const receiverOptions = [
-  { value: 'all', label: '전체' },
-  { value: 'normal', label: '일반 단골(10회 미만)' },
-  { value: 'vip', label: '인증 단골(10회 이상)' },
-];
+import { useMessageForm } from "@hooks/notices/useMessageForm.ts";
+import { receiverOptions } from "@/types/notices.ts";
 
 const NoticesForm = () => {
-  const [receiver, setReceiver] = useState<string | null>(null);
-
+  const {  register, handleSubmit, receiver, onSubmit, onChange  } =useMessageForm();
   return (
     <S.NoticesFormContainer>
       {/* 메세지 제목 */}
       <S.NoticesFormInputSection>
         <S.NoticesFormInputLabel>제목</S.NoticesFormInputLabel>
-        <S.NoticesFormInput type="text" placeholder="제목을 입력하세요" />
+        <S.NoticesFormInput type="text" placeholder="제목을 입력하세요" {...register('title')} />
       </S.NoticesFormInputSection>
       {/* 메세지 수신자 */}
       <S.NoticesFormInputSection>
@@ -25,7 +19,7 @@ const NoticesForm = () => {
           placeholder="받는 사람을 선택하세요"
           options={receiverOptions}
           value={receiver}
-          onChange={setReceiver}
+          onChange={onChange}
         />
       </S.NoticesFormInputSection>
       {/* 메세지 본문 */}
@@ -33,7 +27,7 @@ const NoticesForm = () => {
         <S.NoticesFormInputLabel>본문</S.NoticesFormInputLabel>
         <S.NoticesFormTextArea placeholder="본문을 입력하세요" />
       </S.NoticesFormInputSection>
-      <S.NoticesFormButton>공지 보내기</S.NoticesFormButton>
+      <S.NoticesFormButton onClick={handleSubmit(onSubmit)}>공지 보내기</S.NoticesFormButton>
     </S.NoticesFormContainer>
   );
 };
