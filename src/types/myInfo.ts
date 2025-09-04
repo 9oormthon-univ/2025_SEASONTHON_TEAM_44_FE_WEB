ddc7cb5de8d2e363b9ec28724086e8724b799ef7
Tnalxmsk <alstnwkd990@naver.com>
2025-09-04 17:03:23 +0900

    :sparkles: feat: 내 정보 타입 및 임시 데이터 정의

---
+++ src/types/myInfo.ts
@@ -0,0 +1,33 @@
+import ImgExampleInfo from "@img/img-example-info.png";
+
+export interface MyInfo {
+  profile: {
+    name: string;
+    profileImage: string | undefined;
+    address: string;
+  };
+  storeInfo: {
+    name: string;
+    storeImage: string | undefined;
+    description: string;
+    phoneNumber: string;
+    openingHours: string;
+    address: string;
+  };
+}
+
+export const myInfo: MyInfo = {
+  profile: {
+    name: "다시온님",
+    profileImage: undefined,
+    address: "분당구 판교동",
+  },
+  storeInfo: {
+    name: "다시온 분식",
+    storeImage: ImgExampleInfo,
+    description: "저희집 해물라면이 정말 일품입니다.",
+    phoneNumber: "010-1234-5678",
+    openingHours: "평일 09:00~20:00",
+    address: "경기 성남시 분당구 다시온로177번길 25 2층 220호",
+  },
+};
