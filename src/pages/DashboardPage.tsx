9825076f1431383c3efcb87882853f663728676a
Tnalxmsk <alstnwkd990@naver.com>
2025-09-06 00:53:10 +0900

    :art: style: 대시보드 페이지 ui 구현

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,40 @@
+import NoticeResponseRate from "@components/dashboard/NoticeResponseRate.tsx";
+import RegionalRegularRate from "@components/dashboard/RegionalRegularRate.tsx";
+import TodaySummaryCard from "@components/dashboard/TodaySummaryCard.tsx";
+import TotalQRScans from "@components/dashboard/TotalQRScans.tsx";
+import styled from "@emotion/styled";
+
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <DashboardContainer>
+      <DashboardSection>
+        <TotalQRScans />
+        <TodaySummaryCard />
+      </DashboardSection>
+      <DashboardSection>
+        <RegionalRegularRate />
+        <NoticeResponseRate />
+      </DashboardSection>
+    </DashboardContainer>
   );
 };
 
 export default DashboardPage;
+
+const DashboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-radius: 20px;
+  padding: 20px;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.grayScale.gray50};
+  width: 100%;
+  min-width: 1240px;
+`;
+
+const DashboardSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+`;
