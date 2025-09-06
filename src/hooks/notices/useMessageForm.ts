ce597324604e924ae1ad4fdcfa38650e425c338d
Tnalxmsk <alstnwkd990@naver.com>
2025-09-07 03:22:40 +0900

    :recycle: refactor: 수신자 타입 수정

--- src/hooks/notices/useMessageForm.ts
+++ src/hooks/notices/useMessageForm.ts
@@ -6,7 +6,7 @@ import type { ReceiverOption } from "@/types/notices.ts";
 
 const schema = z.object({
   title: z.string().min(1, "제목을 입력하세요").max(50, "제목은 최대 50자까지 입력 가능합니다."),
-  receiver: z.enum(["all", "normal", "vip"]).nullable().refine(val => val !== null, { message: "받는 사람을 선택하세요" }),
+  receiver: z.enum(["ALL", "BASIC", "CERTIFIED"]).nullable().refine(val => val !== null, { message: "받는 사람을 선택하세요" }),
   message: z.string().min(1, "본문을 입력하세요").max(500, "본문은 최대 500자까지 입력 가능합니다."),
 });
 
@@ -14,7 +14,7 @@ type FormValues = z.infer<typeof schema>;
 
 export const useMessageForm = () => {
   const [receiver, setReceiver] = useState<string | null>(null);
-  const { register, handleSubmit, setValue } = useForm<FormValues>({
+  const { register, handleSubmit, setValue, getValues } = useForm<FormValues>({
     resolver: zodResolver(schema),
   });
 
@@ -27,5 +27,5 @@ export const useMessageForm = () => {
     console.log(data);
   };
 
-  return { register, handleSubmit, receiver, onSubmit, onChange };
+  return { register, handleSubmit, receiver, onSubmit, onChange, getValues };
 }
