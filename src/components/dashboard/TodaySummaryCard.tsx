7355e93ff86d4e375eb958c7d8adb24912d87107
Tnalxmsk <alstnwkd990@naver.com>
2025-09-18 15:52:09 +0900

    :art: style: 오늘 요약 UI 부분 수정

--- src/components/dashboard/TodaySummaryCard.tsx
+++ src/components/dashboard/TodaySummaryCard.tsx
@@ -1,27 +1,35 @@
 import IcArrowUp from "@icon/ic-arrow-up.svg";
 import IcArrowDown from "@icon//ic-arrow-down.svg";
 import * as S from "@components/dashboard/TodaySummaryCard.css.ts";
+import styled from "@emotion/styled";
 
 interface TodaySummaryCardProps {
-  visitors: number
-  diffVisitors: number
-  newRegulars: number
-  diffNewRegulars: number
-  revisitRegulars: number
-  diffRevisitRegulars: number
+  visitors: number;
+  diffVisitors: number;
+  newRegulars: number;
+  diffNewRegulars: number;
+  revisitRegulars: number;
+  diffRevisitRegulars: number;
 }
 
-const TodaySummaryCard = ({ visitors, diffVisitors, diffRevisitRegulars, diffNewRegulars, revisitRegulars, newRegulars }: TodaySummaryCardProps) => {
+const TodaySummaryCard = ({
+                            visitors,
+                            diffVisitors,
+                            diffRevisitRegulars,
+                            diffNewRegulars,
+                            revisitRegulars,
+                            newRegulars,
+                          }: TodaySummaryCardProps) => {
   const isMinusValue = (value: number) => {
     return value < 0;
-  }
+  };
 
   const formatNumber = (value: number) => {
     if (value < 0) {
-      return -value
+      return -value;
     }
     return value;
-  }
+  };
 
   return (
     <S.TodaySummaryCardContainer>
@@ -31,29 +39,64 @@ const TodaySummaryCard = ({ visitors, diffVisitors, diffRevisitRegulars, diffNew
           <div>오늘의 방문자</div>
           <S.TodaySummaryContentItemValueSection isIncrease={!isMinusValue(diffVisitors)}>
             <div>{visitors}</div>
-            <img src={isMinusValue(diffVisitors) ? IcArrowDown : IcArrowUp} alt="" />
-            <div>{formatNumber(diffVisitors)}</div>
+            {diffVisitors === 0 ? <ZeroDiffValueWrapper>
+              <div />
+              <div />
+            </ZeroDiffValueWrapper> : (
+              <>
+                <img src={isMinusValue(diffVisitors) ? IcArrowDown : IcArrowUp} alt="" />
+                <div>{formatNumber(diffVisitors)}</div>
+              </>
+            )}
           </S.TodaySummaryContentItemValueSection>
         </S.TodaySummaryContentItem>
         <S.TodaySummaryContentItem>
           <div>신규 단골 수</div>
           <S.TodaySummaryContentItemValueSection isIncrease={!isMinusValue(diffNewRegulars)}>
             <div>{newRegulars}</div>
-            <img src={isMinusValue(diffNewRegulars) ? IcArrowDown : IcArrowUp} alt="" />
-            <div>{formatNumber(diffNewRegulars)}</div>
+            {diffNewRegulars === 0 ? <ZeroDiffValueWrapper>
+              <div />
+              <div />
+            </ZeroDiffValueWrapper> : (<>
+                <img src={isMinusValue(diffNewRegulars) ? IcArrowDown : IcArrowUp} alt="" />
+                <div>{formatNumber(diffNewRegulars)}</div>
+              </>
+            )}
           </S.TodaySummaryContentItemValueSection>
         </S.TodaySummaryContentItem>
         <S.TodaySummaryContentItem>
           <div>재방문 단골 수</div>
           <S.TodaySummaryContentItemValueSection isIncrease={!isMinusValue(diffVisitors)}>
             <div>{revisitRegulars}</div>
-            <img src={isMinusValue(diffRevisitRegulars) ? IcArrowDown : IcArrowUp} alt=""/>
-            <div>{formatNumber(diffRevisitRegulars)}</div>
+            {diffRevisitRegulars === 0 ? <ZeroDiffValueWrapper>
+              <div />
+              <div />
+            </ZeroDiffValueWrapper> : (
+              <>
+                <img src={isMinusValue(diffRevisitRegulars) ? IcArrowDown : IcArrowUp} alt="" />
+                <div>{formatNumber(diffRevisitRegulars)}</div>
+              </>
+            )}
           </S.TodaySummaryContentItemValueSection>
         </S.TodaySummaryContentItem>
       </S.TodaySummaryContentSection>
     </S.TodaySummaryCardContainer>
-  );
+  )
+    ;
 };
 
 export default TodaySummaryCard;
+
+const ZeroDiffValueWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2px;
+  padding: 15px;
+  margin-left: auto;
+
+  div {
+    height: 2px;
+    width: 8px;
+    background-color: ${({ theme }) => theme.colors.grayScale.gray400};
+  }
+`;
